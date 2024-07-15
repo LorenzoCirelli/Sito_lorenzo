a3d0e0194c2cebf83ae171f081a77e81d2df3bd2
Lorenzo cirelli <88227516+LorenzoCirelli@users.noreply.github.com>
2024-07-15 19:07:18 +0200

    Pagina 2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import home from '@/view/HomePage.vue'
+import Work from './view/WorkPage.vue';
 </script>
 
 <template>
   <home></home>
+  <Work></Work>
 </template>
 
 <style scoped>
--- src/view/WorkPage.vue
+++ src/view/WorkPage.vue
@@ -1,3 +1,116 @@
+<style scoped lang="scss">
+.container_testo {
+  min-width: 150px;
+  max-width: 100%;
+  min-height: 50px;
+  font-size: clamp(25px, 2.3vw, 300px);
+  font-weight: 700;
+  display: flex;
+  padding: clamp(10px, 2vh, 50px) clamp(10px, 2vw, 50px);
+  justify-content: center;
+  &__testo {
+    width: fit-content;
+    text-align: center;
+  }
+}
+.icone::after {
+  position: relative;
+  top: -16svh;
+  right: -18svh;
+  content: '\ead3';
+  font-size: clamp(20px, 3vw, 300px);
+  animation: braces 10s infinite;
+  filter: invert(0.3);
+}
+.icone::before {
+  position: relative;
+  top: -5svh;
+  right: -16svh;
+  content: '\ea24';
+  font-size: clamp(20px, 3vw, 300px);
+  animation: lampadina 5s infinite;
+  filter: invert(0.3);
+}
+
+@keyframes lampadina {
+  0% {
+    text-shadow: 2px 2px 10px rgb(255, 255, 0);
+    top: -16svh;
+    right: -18svw;
+  }
+  25% {
+    text-shadow: 2px 2px 10px rgb(231, 167, 6);
+    text-shadow: 2px 2px 10px transparent;
+    top: -10svh;
+    right: -25svw;
+  }
+  50% {
+    text-shadow: 2px 2px 10px rgb(118, 142, 38);
+    top: -10svh;
+    right: -35svw;
+  }
+  75% {
+    text-shadow: 2px 2px 10px transparent;
+    top: -10svh;
+    right: -30svw;
+  }
+  100% {
+    text-shadow: 2px 2px 10px rgb(255, 255, 0);
+    top: -16svh;
+    right: -18svw;
+  }
+}
+
+@keyframes braces {
+  0% {
+    text-shadow: 2px 2px 10px rgb(31, 163, 207);
+    top: -16svh;
+    right: 18svw;
+  }
+  25% {
+    text-shadow: 2px 2px 10px rgb(15, 144, 190);
+    text-shadow: 2px 2px 10px transparent;
+    top: -10svh;
+    right: 15svw;
+  }
+  50% {
+    text-shadow: 2px 2px 10px rgb(18, 199, 222);
+    top: -20svh;
+    right: 35svw;
+  }
+  75% {
+    text-shadow: 2px 2px 10px transparent;
+    top: -20svh;
+    right: 30svw;
+  }
+  100% {
+    text-shadow: 2px 2px 10px rgb(31, 163, 207);
+    top: -15svh;
+    right: 18svw;
+  }
+}
+.experience {
+  max-width: 100%;
+  min-height: 50px;
+  padding: clamp(20px, 3vh, 400px) clamp(30px, 5vw, 500px);
+  &__bg {
+    background-color: white;
+  min-width: 100%;
+}
+}
+</style>
 <template>
-    <h1>Sviluppando</h1>
+  <div class="container_testo">
+    <div class="container_testo__testo">
+      Progettando <span class="font_medium">e</span> sviluppando
+      <div><span class="font_medium">il</span> <span class="colore">futuro</span></div>
+      <span class="material-symbols-outlined icone"></span>
+    </div>
+  </div>
+
+  <div class="experience">
+    <div class="experience__bg">
+            testo
+    </div>
+  </div>
 </template>
