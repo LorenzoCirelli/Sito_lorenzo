25c2d4b5cdba8b290bee2ebe99f23ca871bb6738
Lorenzo cirelli <88227516+LorenzoCirelli@users.noreply.github.com>
2024-12-18 17:56:00 +0100

    Modifiche normative e update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,56 @@
 <style scoped>
 .law {
   width: 97%;
-  position:fixed;
-  top:0%;
+  position: fixed;
+  top: 0%;
   background-color: rgb(190, 44, 44);
-  padding:1.5%
+  padding: 1.5%;
+}
+
+.blurAndBlock :not(.law, .law > *) {
+  filter: blur(30px);
 }
 </style>
-<script lang="ts" setup>
-import { SpeedInsights } from '@vercel/speed-insights/vue';
-import home from '@/view/HomePage.vue'
-import Work from './view/WorkPage.vue'
-import Vita from './view/VitaPersonale.vue'
-import Contatti from './view/ContattiLorenzo.vue'
-import { useDark } from '@vueuse/core'
-import cookiePrivacy from './view/cookiePrivacy.vue'
-
-
-const isDark = useDark()
-
-</script>
 
 <template>
-  <SpeedInsights />
   <div :class="isDark ? 'dark' : 'light'">
-    <home></home>
-    <Work></Work>
-    <Vita></Vita>
-    <Contatti></Contatti>
+    <span>
+      <home></home>
+      <Work></Work>
+      <Vita></Vita>
+      <Contatti></Contatti>
+    </span>
 
-    <cookiePrivacy class="law"></cookiePrivacy>
+    <cookiePrivacy
+      class="law"
+      @privacy="
+        (stato: boolean) => {
+          if (stato) {
+            accettazione()
+          }
+        }
+      "
+    ></cookiePrivacy>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useDark } from '@vueuse/core'
+import cookiePrivacy from './view/cookiePrivacy.vue'
+import { defineAsyncComponent } from 'vue'
+const isDark = useDark()
+const cookies = document.cookie
+
+if (!cookies.search('policyAccept')) {
+  accettazione()
+}
+
+function accettazione() {
+  ;(window as any).clarity('consent')
+}
+
+const home = defineAsyncComponent(() => import('@/view/HomePage.vue'))
+const Work = defineAsyncComponent(() => import('@/view/WorkPage.vue'))
+const Vita = defineAsyncComponent(() => import('@/view/VitaPersonale.vue'))
+const Contatti = defineAsyncComponent(() => import('@/view/ContattiLorenzo.vue'))
+</script>
--- src/view/cookiePrivacy.vue
+++ src/view/cookiePrivacy.vue
@@ -1,34 +1,41 @@
 <style scoped>
 div {
-    transform: scale(0.9);
+  transform: scale(0.9);
 }
 button {
-    padding:2vh;
-    border: transparent;
-    border-radius: 10px;
-    cursor: pointer;
+  padding: 2vh;
+  margin: 10px 10px 0px 0px;
+  border: transparent;
+  border-radius: 10px;
+  cursor: pointer;
 }
 button:hover {
-    background-color: black;
-    color: wheat;
+  background-color: black;
+  color: wheat;
 }
 </style>
 <template>
-    <div v-if="mostra">
-        <h1>{{ $t('legal[0]') }}</h1>
-        <p>{{ $t('legal[1]') }}</p>
-        <a href="privacyutente.pdf" target="_blank"> terms and conditions </a>"
-        <br>
-        <button @click="nascondi">{{ $t('legal[2]') }}</button>
-    </div>
+  <div v-if="mostra">
+    <h1>{{ $t('legal[0]') }}</h1>
+    <p>{{ $t('legal[1]') }}</p>
+    <a href="privacyutente.pdf" target="_blank"> terms and conditions </a>"
+    <br />
+    <button @click="accetta(true)">{{ $t('legal[2]') }}</button>
+    <button @click="accetta(false)">{{ $t('legal[3]') }}</button>
+  </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-let cookies = document.cookie
-let mostra = cookies.search("policyAccept") ? ref(true) : ref(false);
+import { ref, type Ref } from 'vue'
+const cookies = document.cookie
+let mostra = cookies.search('policyAccept') ? ref(true) : ref(false)
 
-function nascondi() {
-    mostra.value = false;
-    document.cookie = "policyAccept=true";
+const emit = defineEmits(['privacy'])
+
+function accetta(stato: boolean) {
+  mostra.value = false
+  emit('privacy', stato)
+  if (stato) {
+    document.cookie = 'policyAccept=true'
+  }
 }
 </script>
