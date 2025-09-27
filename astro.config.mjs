d1324944da328b38617aec608ab95af601383e16
Lorenzo cirelli <88227516+LorenzoCirelli@users.noreply.github.com>
2025-09-27 18:48:41 +0200

    Added vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 import node from '@astrojs/node';
 
+import vue from "@astrojs/vue";
+
 export default defineConfig({
   output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
   i18n: {
     locales: ["it", "en"],
     defaultLocale: "en",
     routing: { prefixDefaultLocale: true },
   },
+
+  integrations: [vue()],
 });
