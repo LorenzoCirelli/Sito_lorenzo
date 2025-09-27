66d1b2d4e1c05a84150eb9244154eadbbb286062
Lorenzo cirelli <88227516+LorenzoCirelli@users.noreply.github.com>
2025-09-27 23:22:56 +0200

    Added second page of onboarding

---
+++ src/components/icons/IconSelector.vue
@@ -0,0 +1,18 @@
+<style lang="scss" scoped>
+@use '../../assets/styling/designComponents/icon.scss';
+
+</style>
+<template>
+<span class="material-symbols-outlined">
+{{ icon }}
+</span>
+</template>
+<script lang="ts" setup>
+
+defineProps({
+    icon: {
+        type: String,
+        required: true
+    }
+})
+</script>
